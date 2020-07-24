e577e406e0f12e919cd3e99963cb3126ad1eb84d
Magnus Espeland <github@magg.no>
2020-07-24 14:54:53 +0200

    Change name to ScreepsOS

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-js")
+
 }
 
 kotlin.target.nodejs { }
