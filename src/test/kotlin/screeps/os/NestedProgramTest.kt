9327812db7ee9b15abfe83ed0b0b0450ee021fa8
Magnus Espeland <github@magg.no>
2020-08-01 23:40:30 +0200

    Add a test where program A `wait()` inside program B.

--- src/test/kotlin/screeps/os/NestedProgramTest.kt
+++ src/test/kotlin/screeps/os/NestedProgramTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 class NestedProgramTest : KernelTest() {
 
     @Test
-    fun nestedProgram() {
+    fun requestingProgram() {
         val requesterProgram = RequesterProgram()
         kernel.spawnProcess(requesterProgram, 10)
 
@@ -18,4 +18,21 @@ class NestedProgramTest : KernelTest() {
 
         assertEquals(listOf(5,6,7,8,9), requesterProgram.getIds())
     }
+
+    @Test
+    fun moduloProgram() {
+        val moduloProgram = ModuloProgram()
+        kernel.spawnProcess(moduloProgram, 10)
+
+        assertEquals(0, moduloProgram.firstTick)
+        assertEquals(0, moduloProgram.secondTick)
+
+        repeat(10) {
+            kernel.loop()
+        }
+
+        assertEquals(6, moduloProgram.firstTick)
+        assertEquals(9, moduloProgram.secondTick)
+    }
+
 }
--- src/test/kotlin/screeps/os/NestedPrograms.kt
+++ src/test/kotlin/screeps/os/NestedPrograms.kt
@@ -1,6 +1,6 @@
 package screeps.os
 
-class AccumulatorProgram : SleepingProgram() {
+class ProviderProgram : SleepingProgram() {
 
     suspend fun getXExecutionIds(amount: Int) : List<Int> {
         val executionList = mutableListOf<Int>()
@@ -14,28 +14,65 @@ class AccumulatorProgram : SleepingProgram() {
 
         return executionList
     }
+
+    suspend fun waitForTickModuloX(modulo: Int) : Int {
+
+        wait({Kernel.kernel.getTick() % modulo == 0})
+
+        return Kernel.kernel.getTick()
+    }
 }
 
 class RequesterProgram : Program() {
     val ids = mutableListOf<Int>()
 
     override suspend fun execute() {
-        val accumulatorProgram = AccumulatorProgram()
-        val accPid = Kernel.kernel.spawnProcess(accumulatorProgram, 10)
+        val providerProgram = ProviderProgram()
+        val provPid = Kernel.kernel.spawnProcess(providerProgram, 10)
 
-        println("${getProgramName()} Started accumulator program, going to sleep")
+        println("${getProgramName()} Started provider program, going to sleep")
         sleep(5)
 
         val numberOfExecutionIdsToRequest = 5
         println("${getProgramName()} Going to request $numberOfExecutionIdsToRequest execution ids")
 
-        ids.addAll(accumulatorProgram.getXExecutionIds(numberOfExecutionIdsToRequest))
+        ids.addAll(providerProgram.getXExecutionIds(numberOfExecutionIdsToRequest))
 
         println("${getProgramName()} Currently have the following ids: $ids")
 
-        Kernel.kernel.killProcess(accPid)
+        Kernel.kernel.killProcess(provPid)
         exit()
     }
 
     fun getIds() = ids
+}
+
+class ModuloProgram : Program() {
+    var firstTick = 0
+    var secondTick = 0
+
+    override suspend fun execute() {
+        val providerProgram = ProviderProgram()
+        val provPid = Kernel.kernel.spawnProcess(providerProgram, 10)
+
+        println("${getProgramName()} Started provider program, going to sleep")
+        sleep(5)
+
+        val modulo = 3
+        println("${getProgramName()} Getting the first tick that is modulo $modulo")
+
+        firstTick = providerProgram.waitForTickModuloX(modulo)
+        println("${getProgramName()} The first tick that is modulo $modulo is $firstTick")
+
+
+        sleep(1)
+        println("${getProgramName()} Getting the second tick that is modulo $modulo")
+
+        secondTick = providerProgram.waitForTickModuloX(modulo)
+        println("${getProgramName()} The second tick that is modulo $modulo is $secondTick")
+
+        Kernel.kernel.killProcess(provPid)
+        exit()
+    }
+
 }
