0c44e07e3d4fe5ff6f4a82bb37c748711bcd6fdc
Magnus Espeland <github@magg.no>
2020-08-01 22:58:21 +0200

    Split program classes out into separate file, and rename test file

--- src/test/kotlin/screeps/os/BasicKernelTest.kt
+++ src/test/kotlin/screeps/os/BasicKernelTest.kt
@@ -3,7 +3,7 @@ package screeps.os
 import kotlin.test.*
 
 
-class KernelTest {
+class BasicKernelTest {
 
     private lateinit var kernel: Kernel
 
@@ -11,42 +11,6 @@ class KernelTest {
         override var tick = 0
         override var cpu = 0
         override var cpuLimit = 5
-    }
-
-    abstract class TestProgram : Program() {
-        var executions = 0
-
-        protected fun print(nextState: String) {
-            println("${getProgramName()} executing for the ${++executions}. time. $nextState")
-        }
-    }
-
-    class SleepingProgram : TestProgram() {
-        override suspend fun execute() {
-            while(true) {
-                print("Sleeping 1 tick")
-                sleep(1)
-            }
-        }
-    }
-
-    class RunningProgram : TestProgram() {
-        override suspend fun execute() {
-            while(true) {
-                print("Yielding")
-                yield()
-            }
-        }
-    }
-
-    class WaitingProgram(private val condition: () -> Boolean) : TestProgram() {
-        override suspend fun execute() {
-            while(true) {
-                print("Waiting")
-                wait(condition)
-                sleep(1)
-            }
-        }
     }
 
     @BeforeTest
---
+++ src/test/kotlin/screeps/os/BasicPrograms.kt
@@ -0,0 +1,37 @@
+package screeps.os
+
+abstract class TestProgram : Program() {
+    var executions = 0
+
+    protected fun print(nextState: String) {
+        println("${getProgramName()} executing for the ${++executions}. time. $nextState")
+    }
+}
+
+class SleepingProgram : TestProgram() {
+    override suspend fun execute() {
+        while(true) {
+            print("Sleeping 1 tick")
+            sleep(1)
+        }
+    }
+}
+
+class RunningProgram : TestProgram() {
+    override suspend fun execute() {
+        while(true) {
+            print("Yielding")
+            yield()
+        }
+    }
+}
+
+class WaitingProgram(private val condition: () -> Boolean) : TestProgram() {
+    override suspend fun execute() {
+        while(true) {
+            print("Waiting")
+            wait(condition)
+            sleep(1)
+        }
+    }
+}
